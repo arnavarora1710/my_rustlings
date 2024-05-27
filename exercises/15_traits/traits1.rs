c12cd2429ec93e2fb97a1bb29097e3557f11d727
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-05-27 19:29:53 -0400

    done traits1

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -7,14 +7,18 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        let mut x : String = self;
+        x.push_str("Bar".into());
+        x
+    }
 }
 
 fn main() {
